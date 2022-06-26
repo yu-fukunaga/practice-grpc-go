351673106fae637bb181d5d1a04fa5e47ee6e774
yu-fukunaga <92198819+yu-fukunaga@users.noreply.github.com>
2022-06-26 11:03:46 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"pancake.maker/app/handler"
+	"pancake.maker/gen/proto"
+)
+
+func main(){
+	port := 50051
+	lis, err := net.Listen("tcp",fmt.Sprintf(":%d",port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+	}
+
+	server := grpc.NewServer()
+	proto.RegisterPancakeBakerServiceServer(
+		server,
+		handler.NewBakerHandler(),
+	)
+	reflection.Register(server)
+
+	go func(){
+		log.Printf("start gRPC server port: %v\n",port)
+		if err := server.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	quit :=make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	<- quit
+	log.Println("stopping gRPC server...")
+	server.GracefulStop()
+}
